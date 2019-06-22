45372839a3544a6b68d670e4b9fcbf5255da664e
Matt Klein <mklein246@gmail.com>
2019-06-22 00:08:30 -0700

    users table is up and all endpoints work.

--- api/server.js
+++ api/server.js
@@ -2,6 +2,18 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 
+const usersRouter = require('../routers/usersRouter.js');
+
 const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+
+server.use('/users', usersRouter)
+
+server.get('/', (req, res) => {
+  res.status(200).json({ api: "up" })
+})
 
 module.exports = server;
---
+++ routers/usersModel.js
@@ -0,0 +1,53 @@
+const db = require('../data/dbConfig.js');
+
+module.exports = {
+  add,
+  find,
+  findBy,
+  findById,
+  update,
+  remove
+}
+
+function add(user) {
+  return db('users')
+    .returning('id')
+    .insert(user)
+    .then(ids => {
+      [id] = ids;
+      return findById(id);
+    });
+}
+
+function find() {
+  return db('users')
+}
+
+function findBy(filter) {
+  return db('users').where(filter);
+}
+
+function findById(id) {
+  return db('users')
+    .where("id", id)
+    .first();
+}
+
+function update(id, changes) {
+  return db('users')
+    .where("id", id)
+    .update(changes)
+    .then(user => {
+      if(user > 0) {
+        return findById(id);
+      } else {
+        return null;
+      }
+    })
+}
+
+function remove(id) {
+  return db('users')
+    .where("id", id)
+    .del();
+}
