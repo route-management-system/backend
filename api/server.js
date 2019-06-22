7d550d534b1aed32e92617286388b3704d14d1e4
Matt Klein <mklein246@gmail.com>
2019-06-21 21:10:18 -0700

    began project setup

---
+++ api/server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+
+const server = express();
+
+module.exports = server;
---
+++ config/secrets.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'Old King Cole was a merry old soul',
+};
